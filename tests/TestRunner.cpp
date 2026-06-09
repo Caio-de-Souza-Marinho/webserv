433d4bc87cc14c23f6cd3befb51f17d46302c1d5
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2026-06-09 01:28:07 -0300

    add request builder test to runner

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -10,6 +10,7 @@ int	main()
 	testRequestParser();
 	testRouter();
 	testConfigParser();
+	testResponseBuilder();
 
 	std::cout << "\nPassed: " << g_testsPassed << std::endl;
 	std::cout << "Failed: " << g_testsFailed << std::endl;
